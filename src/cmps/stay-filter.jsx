4e76a04150333e4c40af928c206c47509974d828
OritF <oritfad@gmail.com>
2022-05-26 18:15:48 +0300

    add filters

--- src/cmps/stay-filter.jsx
+++ src/cmps/stay-filter.jsx
@@ -1,6 +1,68 @@
 
-export const StayFilter = () => {
+export const StayFilter = ({ onHandleChange, onAmenitiesChange, onLabelChange, onPriceChange }) => {
     return <section className="stay-filter">
-        Filters
+        {/* <button>House</button>
+        <button>Apartment</button>
+        <button>Guesthouse</button>
+        <button>Hotel</button> */}
+        <div>
+            <input type="checkbox" id="House" name="House" value="House" onChange={onHandleChange} />
+            <label htmlFor="House">House</label>
+
+            <input type="checkbox" id="Apartment" name="Apartment" value="Apartment" onChange={onHandleChange} />
+            <label htmlFor="Apartment">Apartment</label>
+
+            <input type="checkbox" id="House" name="Guesthouse" value="Guesthouse" onChange={onHandleChange} />
+            <label htmlFor="Guesthouse">Guesthouse</label>
+
+            <input type="checkbox" id="Hotel" name="Hotel" value="Hotel" onChange={onHandleChange} />
+            <label htmlFor="Hotel">Hotel</label>
+
+        </div>
+
+        <div>
+            <input type="checkbox" id="TV" name="TV" value="TV" onChange={onAmenitiesChange} />
+            <label htmlFor="TV">TV</label>
+
+            <input type="checkbox" id="Wifi" name="Wifi" value="Wifi" onChange={onAmenitiesChange} />
+            <label htmlFor="Wifi">Wifi</label>
+
+            <input type="checkbox" id="Kitchen" name="Kitchen" value="Kitchen" onChange={onAmenitiesChange} />
+            <label htmlFor="Kitchen">Kitchen</label>
+
+            <input type="checkbox" id="Pool" name="Pool" value="Pool" onChange={onAmenitiesChange} />
+            <label htmlFor="Pool">Pool</label>
+
+            <input type="checkbox" id="Air conditioning" name="Air conditioning" value="Air conditioning" onChange={onAmenitiesChange} />
+            <label htmlFor="Air conditioning">Air conditioning</label>
+
+            <input type="checkbox" id="Hot tub" name="Hot tub" value="Hot tub"price />
+            <label htmlFor="Hot tub">Hot tub</label>
+
+            <input type="checkbox" id="Gym" name="Gym" value="Gym" onChange={onAmenitiesChange} />
+            <label htmlFor="Gym">Gym</label>
+
+        </div>
+        <div>
+            <input class="Cabins" id="Cabins" type="button" value="Cabins" onClick={onLabelChange} />
+        </div>
+        <div>
+                <label for="price">price</label>
+            <span>100</span>
+            <input type="range" id="price" name="price" min="100" max="10000" onChange={onPriceChange}/>
+            <span>10000</span>
+        </div>
+
+
+
+
+        {/*         
+        <select name="types" id="types" multiple onChange={onHandleChange}>
+            <option value="House" name="House">House</option>
+            <option value="Apartment" name="Apartment">Apartment</option>
+            <option value="Guesthouse" name="Guesthouse">Guesthouse</option>
+            <option value="Hotel" name="Hotel">Hotel</option>
+        </select> */}
+
     </section>
 }
--- src/pages/stay-app.jsx
+++ src/pages/stay-app.jsx
@@ -1,13 +1,16 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { loadStays } from '../store/actions/stay.actions'
+import { loadStays, setFilter } from '../store/actions/stay.actions'
 
 import { StayList } from '../cmps/stay-list'
 import { StayFilter } from '../cmps/stay-filter'
 
 export const StayApp = () => {
 
-    const { stays } = useSelector((storeState) => storeState.stayModule)
+    const { stays, filterBy } = useSelector((storeState) => storeState.stayModule)
+    const [filterByType, setFilterByType] = useState([])
+    const [filterByAmenities, setFilterByAmenities] = useState([])
+    
     const dispatch = useDispatch()
 
     useEffect(() => {
@@ -15,9 +18,84 @@ export const StayApp = () => {
         dispatch(loadStays())
     }, [])
 
+    useEffect(()=>{
+        dispatch(loadStays())
+        console.log(filterBy)
+    }, [filterBy])
+
+    useEffect(()=>{
+        if (!filterByType) return
+        dispatch(setFilter({...filterBy, type: filterByType}))
+    }, [filterByType])
+
+    useEffect(()=>{
+        dispatch(setFilter({...filterBy, amenities: filterByAmenities}))
+    }, [filterByAmenities])
+
+    const onHandleChange = ({ target }) => {
+        console.log({target})
+        const field = target.name
+        let { value } = target
+        console.log({value})
+        console.log({field})
+
+        // if (field==='House'||field==='Apartment'||field==='Guesthouse'||field==='Hotel')
+        // {
+            if (target.checked){
+                setFilterByType((prevState)=>(
+                    [...prevState,field]
+                ))
+            }
+
+            if (!target.checked){
+                setFilterByType((prevState)=>(
+                    prevState.filter(type => type!==field)
+                ))
+            }
+        // }
+    }
+    const onAmenitiesChange = ({ target }) => {
+        console.log({target})
+        const field = target.name
+        let { value } = target
+        console.log({value})
+        console.log({field})
+
+            if (target.checked){
+                setFilterByAmenities((prevState)=>(
+                    [...prevState,field]
+                ))
+            }
+
+            if (!target.checked){
+                setFilterByAmenities((prevState)=>(
+                    prevState.filter(amenity => amenity!==field)
+                ))
+            }
+    }
+
+    const onLabelChange = ({ target }) => {
+        console.log({target})
+    }
+    const onPriceChange = ({ target }) => {
+        console.log({target})
+    }
+
+
+
+    // const onClearFilter = () => {
+    //     const filterBy = {
+    //         type: []
+    //     }
+    //     dispatch(setFilter(filterBy))
+    // }
 
     return <section className="stay-app">
-        <StayFilter/>
+        <StayFilter filterBy={filterBy} onHandleChange={onHandleChange} 
+        onAmenitiesChange={onAmenitiesChange} 
+        onLabelChange={onLabelChange}
+        onPriceChange={onPriceChange}
+        />
         <StayList stays={stays} />
     </section>
 }
