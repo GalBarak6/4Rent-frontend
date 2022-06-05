e65aa2107df8208e634778864715290dca9ed418
OritF <oritfad@gmail.com>
2022-06-05 16:10:39 +0300

    search: render dates

--- src/cmps/search-calender-modal.jsx
+++ src/cmps/search-calender-modal.jsx
@@ -4,26 +4,31 @@ import { stayService } from '../services/stay.service'
 import { DatePicker } from './date-range'
 import { DateRange } from 'react-date-range'
 
-export const SearchCalenderModal = () => {
-    const [dateRange, setDateRange] = useState([
-        {
-          startDate: new Date(),
-          endDate: new Date(new Date().getTime() + (120 * 60 * 60 * 1000)), //tomorrow
-          key: 'selection',
-        },
-      ])
+export const SearchCalenderModal = ({dateRange, handleDatesChange}) => {
+    // const [dateRange, setDateRange] = useState([
+    //     {
+    //       startDate: new Date(),
+    //       endDate: new Date(),
+    //       key: 'selection',
+    //     },
+    //   ])
 
-    // const onHandleDates = (startDate, endDate) => {
-    //     console.log(startDate);
-    //     console.log(endDate);
-        // setDateRange(prevDates => ({ ...prevDates, startDate, endDate }))
-    // }
+      const onHandleChange = (item) => {
+          console.log(item)
+        // setDateRange([item.selection])
+        const {startDate} = item.selection
+        const {endDate} = item.selection
+        handleDatesChange(startDate,endDate)
+      }
+
+
 
     return <section className="search-calender-modal">
         {/* <DatePicker onHandleDates={onHandleDates}/> */}
         <DateRange
             editableDateInputs={true}
-            // onChange={onHandleChange}
+            onChange={onHandleChange}
+            // onChange={handleDatesChange}
             moveRangeOnFirstSelection={false}
             ranges={dateRange}
             rangeColors={['#222222']}
